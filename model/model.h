6f4dfa5b6c52bc6e20be24c765e0894e2f09ea78
Luigi Ketter <luigiket@si-h5.kzn.21-school.ru>
2022-12-14 20:35:21 +0300

    need to 1 pattern, save settings and delete segfault

--- model/model.h
+++ model/model.h
@@ -22,14 +22,15 @@ public:
         if (!model_) model_ = new Model();
         return model_;
     }
+
   /* functions for parsing .obj */
   bool is_digit(char expression);
   bool count_vertexes_polygons(std::string &path_of_file, data_t &some_data);
   bool create_matrix_obj(std::string &path_of_file, data_t &some_data);
   bool note_vertexes_polygons(std::string &path_of_file, data_t &some_data);
-
   void help_funk_vertexes_polygons(std::string &lineptr, data_t &some_data,
                                    int &count_polygon);
+
   /* functions for transforming object */
   void move_obj(data_t &some_data, double x, double y, double z);
   void rotation_by_ox(data_t &some_data, double corner);
